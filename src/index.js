1f0d106e15b65f965f230d1e308502e18c05f5ad
Ярошук Анастасия Викторовна <99-IaroshukAV@bgs.insu>
2019-02-20 17:22:15 +0300

    Love traingles

--- src/index.js
+++ src/index.js
@@ -4,4 +4,13 @@
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
   // your implementation
+  let count = 0;  
+    for (let i = 0; i<preferences.length; i++) { 
+        let n1 = preferences[i] - 1;
+        let n2 = preferences[n1] - 1;
+        let n3 = preferences[n2] - 1;
+        if (n1 == i || n1 == n2 || n3 == n2) continue;
+        if (n3 == i) count++;
+     }
+     return count / 3;
 };
